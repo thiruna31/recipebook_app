e3a773028eaf9c67e7f50aadcff076bd979e2bd1
thiruna31 <plthirunavukkarasu01@gmail.com>
2025-10-03 13:58:48 -0400

    detail screen

---
+++ lib/deatail_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatefulWidget {
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final recipe = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(recipe['name']!)),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Ingredients: ${recipe['ingredients']}"),
+            SizedBox(height: 10),
+            Text("Instructions: ${recipe['instructions']}"),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
+              child: Text(isFavorite ? "Unfavorite" : "Favorite"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
