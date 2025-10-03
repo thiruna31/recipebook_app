afde9f1b0e615715a682b718a7bd663d6d2426a2
thiruna31 <plthirunavukkarasu01@gmail.com>
2025-10-03 14:44:11 -0400

    Enhanced home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,9 +2,24 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   final List<Map<String, String>> recipes = [
-    {'name': 'Pizza', 'ingredients': 'Flour, Cheese, Tomato', 'instructions': 'Bake for 20 mins'},
-    {'name': 'Pasta', 'ingredients': 'Pasta, Sauce, Cheese', 'instructions': 'Boil and mix'},
-    {'name': 'Cake', 'ingredients': 'Flour, Sugar, Eggs', 'instructions': 'Bake for 30 mins'},
+    {
+      'name': 'Pizza',
+      'ingredients': 'Flour, Cheese, Tomato',
+      'instructions': 'Bake for 20 mins',
+      'image': 'assets/images/pizza.png',
+    },
+    {
+      'name': 'Pasta',
+      'ingredients': 'Pasta, Sauce, Cheese',
+      'instructions': 'Boil and mix',
+      'image': 'assets/images/pasta.png',
+    },
+    {
+      'name': 'Cake',
+      'ingredients': 'Flour, Sugar, Eggs',
+      'instructions': 'Bake for 30 mins',
+      'image': 'assets/images/cake.png',
+    },
   ];
 
   @override
@@ -15,6 +30,12 @@ class HomeScreen extends StatelessWidget {
         itemCount: recipes.length,
         itemBuilder: (context, index) {
           return ListTile(
+            leading: Image.asset(
+              recipes[index]['image']!,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
             title: Text(recipes[index]['name']!),
             onTap: () {
               Navigator.pushNamed(
